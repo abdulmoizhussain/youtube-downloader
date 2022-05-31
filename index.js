b4ad67b593d9949fd89b5f8298e5f07fbe2f6637
Abdul Moiz <moizhus123@gmail.com>
2022-06-01 01:51:11 +0500

    added media files in gitignore. now we can select mediatype of the files to download.

--- index.js
+++ index.js
@@ -1,49 +1,95 @@
 const config = require("./config.json");
+const cliProgress = require('cli-progress');
 const fs = require("fs");
 const ytdl = require("ytdl-core");
-const cliProgress = require('cli-progress');
-const { getInfo, getBasicInfo, } = ytdl;
+const { getInfo } = ytdl;
+const Readline = require("readline");
 const sample = require("./basic_info_sample.json");
 
 const BYTES_IN_ONE_MB = 1048576;
 
+/**
+ * @type {ytdl.videoFormat[]}
+ */
+let formats;
+
+/**
+ * @type {ytdl.videoInfo}
+ */
+let videoInfo;
+
+const progressBar = new cliProgress.SingleBar({
+  format: 'Downloading [{bar}] {percentage}% | {totalDownloadedMegaBytes}/{totalMegaBytes} MB'
+}, cliProgress.Presets.shades_classic);
+
+const readline = Readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 (async function () {
-  sample.formats.forEach(v => console.log(v.itag, v.quality, v.qualityLabel, v.mimeType, v.contentLength));
+  console.log("\nTrying to fetching video information..");
+  videoInfo = await getInfo(config.video_url);
 
-  const progressBar = new cliProgress.SingleBar({
-    // format: 'progress [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}'
-    format: 'Downloading [{bar}] {percentage}% | {totalDownloadedMegaBytes}/{totalMegaBytes} MB'
-  }, cliProgress.Presets.shades_classic);
-  // const info = await getBasicInfo(config.video_url);
+  console.log("Video Title:", videoInfo.videoDetails.title, "\n");
 
-  console.log("Fetching video information..");
-  console.time("getInfo");
-  const info = await getInfo(config.video_url);
-  console.timeEnd("getInfo");
+  formats = videoInfo.formats.map((v, i) => {
+    const regexMatch = v.mimeType.match(/([a-zA-Z0-9]+)\/([a-zA-Z0-9]+);*/);
+    const mediaType = regexMatch[1];
+    const fileFormat = regexMatch[2];
+    const sizeInMb = (v.contentLength / BYTES_IN_ONE_MB).toFixed(3);
+    const qualityLabel = (v.qualityLabel || "").padEnd(11, ' ');
 
+    console.log(
+      i.toString().padStart(3, " "),
+      v.itag.toString().padEnd(3, ' '),
+      qualityLabel,
+      (sizeInMb + " MB").padStart(12, ' '),
+      v.mimeType,
+    );
 
-  // for windows file naming.
-  const cleanedTitle = info.videoDetails.title.replace(/[\/\\\:\*\?\"\<\>\|]+/g, "");
-  const filePath = `./${cleanedTitle}.mp4`;
+    return {
+      itag: v.itag,
+      quality: v.quality,
+      qualityLabel: qualityLabel,
+      mimeType: v.mimeType,
+      mediaType: mediaType,
+      fileFormat: fileFormat,
+    };
+  });
 
-  progressBar.start(100, 0, { totalDownloadedMegaBytes: 0, totalMegaBytes: 0 });
-
-  // ytdl(config.video_url, info)
-  ytdl.downloadFromInfo(info)
-    .on("end", onDownloadEnd.bind(null, progressBar))
-    .on("progress", onProgressCallback.bind(null, progressBar))
-    .pipe(fs.createWriteStream(filePath));
+  readline.question("\nType format-index and enter: ", downloadVideoWithSelectedFormat);
 
 })().catch(console.log);
 
 /**
- * 
- * @param {cliProgress.SingleBar} progressBar 
- * @param {Number} _ 
- * @param {Number} totalBytesDownloaded 
- * @param {Number} totalBytes 
+ * @param {String} userInputFormatIndex
  */
-function onProgressCallback(progressBar, _, totalBytesDownloaded, totalBytes) {
+function downloadVideoWithSelectedFormat(userInputFormatIndex) {
+  const selectedIndex = parseInt(userInputFormatIndex);
+  const format = formats[selectedIndex];
+  const { fileFormat, itag } = format;
+
+  // for windows file naming.
+  const cleanedTitle = videoInfo.videoDetails.title.replace(/[\/\\\:\*\?\"\<\>\|]+/g, "");
+  const filePath = `./${cleanedTitle}-${itag}.${fileFormat}`;
+
+  console.log("\n", "Saving file as: ", filePath, "\n");
+
+  progressBar.start(100, 0, { totalDownloadedMegaBytes: 0, totalMegaBytes: 0 });
+
+  ytdl.downloadFromInfo(videoInfo, { quality: format.itag })
+    .on("end", onDownloadEnd)
+    .on("progress", onProgressCallback)
+    .pipe(fs.createWriteStream(filePath));
+}
+
+/**
+ * @param {Number} _
+ * @param {Number} totalBytesDownloaded
+ * @param {Number} totalBytes
+ */
+function onProgressCallback(_, totalBytesDownloaded, totalBytes) {
 
   const percentage = parseInt((totalBytesDownloaded / totalBytes) * 100);
 
@@ -51,14 +97,9 @@ function onProgressCallback(progressBar, _, totalBytesDownloaded, totalBytes) {
   const totalMegaBytes = (totalBytes / BYTES_IN_ONE_MB).toFixed(3);
 
   progressBar.update(percentage, { totalDownloadedMegaBytes, totalMegaBytes });
-
-  // console.log(`${percentage}% - ${totalDownloadedMegaBytes}/${totalMegaBytes} MB`);
 }
 
-/**
- * 
- * @param {cliProgress.SingleBar} progressBar
- */
-function onDownloadEnd(progressBar) {
+function onDownloadEnd() {
   progressBar.stop();
+  readline.close();
 }
