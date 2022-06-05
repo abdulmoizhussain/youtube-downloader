f6e3eeec0cb9969c82db98f655953824db80c901
Abdul Moiz <moizhus123@gmail.com>
2022-06-05 12:38:20 +0500

    using inquirer.js library instead of prompts library. not using readline as well. creating a build is remaining.

--- index.js
+++ index.js
@@ -1,85 +1,71 @@
 // sources:
 // how to add cookies: https://github.com/fent/node-ytdl-core/blob/master/example/cookies.js
 
-const config = require("./config.json");
 const cliProgress = require('cli-progress');
 const fs = require("fs");
 const ytdl = require("ytdl-core");
+const inquirer = require('inquirer');
 const { getInfo } = ytdl;
-const Readline = require("readline");
-const sample = require("./basic_info_sample.json");
 const winston = require("winston");
-const { format } = winston;
+const { format: winstonFormat } = winston;
 
 const logger = winston.createLogger({
   transports: [
     // new winston.transports.Console(),
     new winston.transports.File({
       filename: './logs/combined.log',
-      format: format.combine(format.timestamp({}), format.splat(), format.simple())
+      format: winstonFormat.combine(winstonFormat.timestamp(), winstonFormat.splat(), winstonFormat.simple())
     })
   ]
 });
 
 const BYTES_IN_ONE_MB = 1048576;
 
-/**
- * @type {ytdl.videoInfo}
- */
-let videoInfo;
-
 const progressBar = new cliProgress.SingleBar({
   format: 'Downloading [{bar}] {percentage}% | {totalDownloadedMegaBytes}/{totalMegaBytes} MB'
 }, cliProgress.Presets.shades_classic);
 
-const readline = Readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const question = require("util").promisify(readline.question).bind(readline);
 
 (async function () {
-  const video_url = await question("\nEnter video URL: ");
+  const { value: video_url } = await inquirer.prompt([{ type: "input", name: "value", message: "Enter video link/URL:" }]);
 
   console.log("\nTrying to fetch video information..");
   logger.info("Trying to fetch video information: %s", video_url);
 
-  videoInfo = await getInfo(video_url);
+  const videoInfo = await getInfo(video_url);
 
   console.log("\nVideo found! Title:", videoInfo.videoDetails.title, "\n");
   logger.info("Video found! Title: %s", videoInfo.videoDetails.title);
 
-  videoInfo.formats.forEach((v, i) => {
-    const regexMatch = v.mimeType.match(/([a-zA-Z0-9]+)\/([a-zA-Z0-9]+);*/);
+  const choices = videoInfo.formats.map((format, index) => {
+    const regexMatch = format.mimeType.match(/([a-zA-Z0-9]+)\/([a-zA-Z0-9]+);*/);
     const mediaTypeAndContainer = regexMatch[0];
-    const sizeInMb = (v.contentLength / BYTES_IN_ONE_MB).toFixed(3);
-    const qualityLabel = (v.qualityLabel || "").padEnd(11, ' ');
+    const sizeInMb = (format.contentLength / BYTES_IN_ONE_MB).toFixed(3);
+    const qualityLabel = (format.qualityLabel || "").padEnd(11, ' ');
 
-    console.log(
-      i.toString().padStart(3, " "),
-      v.itag.toString().padEnd(3, ' '),
+    const choiceMessageSplitted = [
+      // i.toString().padStart(3, " "), // index
+      // v.itag.toString().padEnd(3, ' '), // itag
       qualityLabel,
       (sizeInMb + " MB").padStart(12, ' '),
       mediaTypeAndContainer,
-      v.hasAudio ? "" : "(no audio)"
-    );
+      format.hasAudio ? "" : "(no audio)",
+    ];
+
+    return { name: choiceMessageSplitted.join(" "), value: index };
   });
 
-  readline.question("\nType format-index and enter: ", downloadVideoWithSelectedFormat);
+  const { value: selectedIndex } = await inquirer.prompt([{
+    choices: choices,
+    type: "list",
+    name: "value",
+    message: "Select media type and format:",
+  }]);
 
-})().catch(catcher);
-
-
-/**
- * @param {String} userInputFormatIndex
- */
-function downloadVideoWithSelectedFormat(userInputFormatIndex) {
-  const selectedIndex = parseInt(userInputFormatIndex);
   const format = videoInfo.formats[selectedIndex];
   const { itag, container } = format;
 
-  // for windows file naming.
+  // to follow windows file naming convention.
   const cleanedTitle = videoInfo.videoDetails.title.replace(/[\/\\\:\*\?\"\<\>\|]+/g, "");
   const filePath = `./${cleanedTitle}-${itag}.${container}`;
 
@@ -92,7 +78,8 @@ function downloadVideoWithSelectedFormat(userInputFormatIndex) {
     .on("end", onDownloadEnd)
     .on("progress", onProgressCallback)
     .pipe(fs.createWriteStream(filePath));
-}
+
+})().catch(catcher);
 
 /**
  * @param {Number} _
@@ -111,13 +98,12 @@ function onProgressCallback(_, totalBytesDownloaded, totalBytes) {
 
 function onDownloadEnd() {
   progressBar.stop();
-  readline.close();
 }
 
-
 function catcher(error) {
-  progressBar.stop();
-  readline.close();
+  if (progressBar) {
+    progressBar.stop();
+  }
   console.log(error);
-  logger.log(error);
+  logger.error(error);
 }
